a724a177ce20efb5ae4cc88ee3c8aa0f06a7da5e
Ben <benpeterjackson@gmail.com>
2015-06-21 23:18:33 +0100

    updated demos

---
+++ demos/color.wheel.js
@@ -0,0 +1,40 @@
+
+var Scene = require('scene');
+
+var scene = new Scene.Scene({ background: { r: 16, g: 16, b: 16 } });
+
+var wheelRadius = 300,
+    colourWheel = [
+        { r: 255, g: 0, b: 0 },
+        { r: 255, g: 125, b: 0 },
+        { r: 255, g: 255, b: 0 },
+        { r: 125, g: 255, b: 0 },
+        { r: 0, g: 255, b: 0 },
+        { r: 0, g: 255, b: 125 },
+        { r: 0, g: 255, b: 255 },
+        { r: 0, g: 125, b: 255 },
+        { r: 0, g: 0, b: 255 },
+        { r: 125, g: 0, b: 255 },
+        { r: 255, g: 0, b: 255 },
+        { r: 255, g: 0, b: 125 }
+    ];
+
+colourWheel.forEach(function(colour, i) {
+
+    var angle = i * Math.PI/6;
+
+    scene.addSurface(new Scene.Sphere(
+        { x: wheelRadius * Math.sin(angle), y: wheelRadius * Math.cos(angle), z: -2800 }, 
+        75, 
+        {
+            color: colour, specularExp: 1000
+        }));
+
+});
+
+scene.addSurface(new Scene.Sphere({ x: 0, y: 0, z: -3000 }, 200, {
+    color: { r: 255, g: 255, b: 255 }, reflectiveness: 0.4
+}));
+
+scene.addLight({ x: 450, y: 175, z: -2600 }, 0.8);
+scene.render(document.querySelector('canvas'));
--- demos/greyscale.js
+++ demos/greyscale.js
@@ -3,13 +3,13 @@ var Scene = require('scene');
 
 var scene = new Scene.Scene({ background: { r: 100, g: 100, b: 100 } });
 
-var sphere = new Scene.Sphere({ x: -300, y: 200, z: -2800 }, 80, {
+var topSphere = new Scene.Sphere({ x: -300, y: 200, z: -2800 }, 80, {
         color: { r: 160, g: 160, b: 160 }, specularExp: 10000
     }),
-    sphere2 = new Scene.Sphere({ x: -300, y: 40, z: -2800 }, 80, {
+    middleSphere = new Scene.Sphere({ x: -300, y: 40, z: -2800 }, 80, {
         color: { r: 200, g: 200, b: 200 }, specularExp: 10000
     }),
-    sphere3 = new Scene.Sphere({ x: -300, y: -120, z: -2800 }, 80, {
+    bottomSphere = new Scene.Sphere({ x: -300, y: -120, z: -2800 }, 80, {
         color: { r: 45, g: 45, b: 45 }, specularExp: 10000
     }),
     mirror = new Scene.Sphere({ x: 400, y: 400, z: -5000 }, 500, {
@@ -21,9 +21,9 @@ var sphere = new Scene.Sphere({ x: -300, y: 200, z: -2800 }, 80, {
         { color: { r: 80, g: 80, b: 80},  reflectiveness: 0.1 }
     );
 
-scene.addSurface(sphere);
-scene.addSurface(sphere2);
-scene.addSurface(sphere3);
+scene.addSurface(topSphere);
+scene.addSurface(middleSphere);
+scene.addSurface(bottomSphere);
 scene.addSurface(mirror);
 scene.addSurface(plane);
 
